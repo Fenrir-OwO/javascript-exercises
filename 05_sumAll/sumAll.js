651319f22b9468070f17afe0416fc074f5cfe1c5
Fenrir-OwO <royshuvo87@gmail.com>
2024-01-14 23:23:14 +0600

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if(!(typeof start === "number" && typeof end === "number") || (start <= 0 || end <= 0)) {
+        return("ERROR"); //checks if any other type than number, and if positive
+    }
+    if(start>end){
+        [start,end] = [end, start]; //swaps position
+    }
+    
+    
 
+    let sum = 0
+    for (i = start; i <= end ;i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
