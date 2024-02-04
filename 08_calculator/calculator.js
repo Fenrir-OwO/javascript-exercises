e0875672e567779558154844442f854c5b0efc89
Fenrir-OwO <royshuvo87@gmail.com>
2024-02-04 22:37:51 +0600

    rest of the JS excercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a- b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {return numbers.reduce((concataneted, current)=>  concataneted + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function factorial(number) {
+  if (number === 0 || number === 1){return 1} //base case factorial of 0 and 1 is always 1
+  else{
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
